0a2815829175379823869fe0ab4efbd3ac0e8acb
SoyUnCitrico <1darenastolteca@gmail.com>
2021-05-03 10:48:26 -0500

    Estructura de carpetas para sitio web

---
+++ scripts/Clase2/Ejemplo4.js
@@ -0,0 +1,27 @@
+//Creamos variable global para contener la información
+let data;
+let contador = 0;
+//El archivo puede tardar en cargar poniéndolo en el método preload
+//aseguramos que  nuestro programa comience una vez que lo cargamos
+function preload(){
+  data =  loadJSON("/scripts/data/data.json")
+}
+
+function setup() {
+  let cnv = createCanvas(800, 600);
+  cnv.parent('main__sketch');
+}
+
+function draw() {
+    //Disminuimos la velocidad de draw a 1 frame por segundo
+  frameRate(1);
+  //Si nuestro contador alcanza el final de elementos, se reinicia
+  if(contador>=data.colors.length){
+    contador = 0;
+  }
+  //Pintamos el fondo con el color de la base de datos
+  background(data.colors[contador].hex);
+  //Utilizamos el texto con el color de la base de datos
+  text(data.colors[contador].color,width/2,height/2)
+  contador++;
+}
---
+++ scripts/Clase3/Ejemplo6.js
@@ -0,0 +1,36 @@
+//Para este ejemplo es necesaria una cuenta en OpenWeatherMap.org y obtener la API KEY
+
+let key = '7c59ef7f3557f92679933dafa19e09fd';//Sustituir por API Key
+//URL para descargar clima actual en Ciudad de México
+let url = 'https://api.openweathermap.org/data/2.5/weather?q=Mexico&appid='+key; 
+//Crear variables para guardar datos
+let data;
+let clima;
+//Variable para texto
+let texto;
+
+function setup() {
+  let cnv = createCanvas(800, 600);
+  cnv.parent('main__sketch');
+  //Cargo información y una vez cargada ejecuto la función pedirClima()
+  loadJSON(url,pedirClima);
+  //Formato de texto
+  textAlign(CENTER);
+
+}
+
+function draw() {
+  background(220);
+  //Texto al centro de la pantalla
+  text(texto,width/2,height/2);
+}
+
+//Esta función se ejecuta una vez que se cargan los datos
+function pedirClima(data){
+  clima = data;
+  console.log(clima);
+  //Acceder a la información de temperatura
+  let temperatura = clima.main.temp;
+  //Concateno el texto a mostrar
+  texto = "La temperatura en CDMX es: "+ temperatura+"°C";
+}
